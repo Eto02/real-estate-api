cba6169d9be4db02ae257c995bc0be43116479ae
Tahta Failah Mubarak <mmikoto89@gmail.com>
2024-07-03 22:45:52 +0700

    feat: add socket server

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { io } from "./src/application/socket.js";
 import { server } from "./src/application/web.js";
 
 const port = 8000;
@@ -5,3 +6,4 @@ const port = 8000;
 server.listen(port, () => {
   console.log("Server is running!!", port);
 });
+io.listen(4000);
